f770b2f6387cd9db0e141413b1745b4554254c61
fubao <freadream@gmail.com>
2020-08-14 19:14:33 -0400

    add_file

---
+++ randomForest_scratch/decision_tree_classifier_from_scratch.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 09:46:52 2020
+
+@author: fubao
+"""
+
+# decision tree implementing from stratch
+
+#  binary classifier
+
+
+class decision_tree(object):
+    
+    def __init__(object):
+        pass
+        
+    
